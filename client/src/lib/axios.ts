e2d9af74bde568eeb6a5c5314675148dadf73cb0
DmytroSheiko <sheykodmitriyjob@gmail.com>
2026-05-22 00:40:57 +0300

    feat: add env for api endpoint

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,8 +1,8 @@
-import axios from 'axios';
-import { getToken, removeToken } from '../shared/utils/token';
+import axios from "axios";
+import { getToken, removeToken } from "../shared/utils/token";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001/api",
 });
 
 api.interceptors.request.use((config) => {
@@ -18,7 +18,7 @@ api.interceptors.response.use(
   (error) => {
     if (error.response?.status === 401) {
       removeToken();
-      window.location.href = '/login';
+      window.location.href = "/login";
     }
     return Promise.reject(error);
   },
